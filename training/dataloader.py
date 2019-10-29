c325e70363a988a05a202fd01b2a196113d1a3ba
jan <s7jaquak@uni-bonn.de>
2019-10-29 14:16:11 +0100

    Normalization parameters for dataset

--- training/dataloader.py
+++ training/dataloader.py
@@ -6,7 +6,6 @@ Authors: Yung-Yu Chen, Jan Quakernack
 import numpy as np
 import torch
 from PIL import Image
-from utils import visualize
 from pathlib import Path
 
 from training import load_config, DATA_DIR
@@ -30,6 +29,7 @@ class SugarBeetDataset(Dataset):
     def __init__(self,
                  label_background, label_ignore, label_weed, label_sugar_beet,
                  input_height, input_width, target_height, target_width,
+                 mean_rgb, mean_nir, std_rgb, std_nir,
                  path_to_rgb_images, suffix_rgb_images,
                  path_to_nir_images, suffix_nir_images,
                  path_to_semantic_labels, suffix_semantic_labels,
@@ -63,6 +63,9 @@ class SugarBeetDataset(Dataset):
         # transformations
         self.resize_input = transforms.Resize((input_height, input_width), interpolation=Image.BILINEAR)
         self.resize_target = transforms.Resize((target_height, target_width), interpolation=Image.NEAREST)
+
+        self.rgb_normalization = transforms.Normalize(mean=mean_rgb, std=std_rgb, inplace=True)
+        self.nir_normalization = transforms.Normalize(mean=(mean_nir,), std=(std_nir,), inplace=True)
 
         # TODO random transformations for data augmentation
 
@@ -151,12 +154,13 @@ class SugarBeetDataset(Dataset):
         # random transformations
         # TODO keep in mind that we need to be able to transform stem positions (x, y) in the same way
 
-        # normalization
-        # TODO
-
         # convert input images to tensors
         rgb_tensor = self.pil_to_tensor(rgb_image) # shape (3, input_height, input_width,)
         nir_tensor = self.pil_to_tensor(nir_image) # shape (1, input_height, input_width,)
+
+        # normalization
+        rgb_tensor = self.rgb_normalization(rgb_tensor)
+        nir_tensor = self.nir_normalization(nir_tensor)
 
         # concatenate RGB and NIR to single input tensor
         input_tensor = torch.cat([rgb_tensor, nir_tensor], dim=0)
@@ -171,7 +175,7 @@ class SugarBeetDataset(Dataset):
         """Remap class labels.
         """
         semantic_target = np.zeros_like(semantic_label)
-        # semantic_target = np.where(semantic_label==self.label_ignore, 1, semantic_target)
+        semantic_target = np.where(semantic_label==self.label_ignore, 1, semantic_target)
         semantic_target = np.where(semantic_label==self.label_weed, 1, semantic_target)
         semantic_target = np.where(semantic_label==self.label_sugar_beet, 2, semantic_target)
 
@@ -189,59 +193,3 @@ class SugarBeetDataset(Dataset):
         return len(self.filenames)
 
 
-# class SugarBeetDataset(object):
-
-    # def __init__(self, root, transforms):
-        # self.root = root
-        # self.transforms = transforms
-        # # load all image files, sorting them to
-        # # ensure that they are aligned
-        # self.imgs = list(sorted(os.listdir(os.path.join(root, "images/rgb"))))
-
-
-    # def __getitem__(self, idx):
-        # # load images ad masks
-        # img_path = os.path.join(self.root, "images/rgb/", self.imgs[idx])
-        # mask_path = os.path.join(self.root, "annotations/PNG/pixelwise/iMap", "{0}_GroundTruth_iMap.png".format(self.imgs[idx].split(".")[0]))
-
-        # img = Image.open(img_path).convert("RGB")
-        # img = np.array(img)
-        # # note that we haven't converted the mask to RGB,
-        # # because each color corresponds to a different instance
-        # # with 0 being background
-        # mask = Image.open(mask_path)
-        # # convert the PIL Image into a numpy array
-        # mask = np.array(mask)
-        # # instances are encoded as different colors
-        # obj_ids = np.array([0,1, 10000, 20001])
-        # # first id is the background, so remove it
-        # obj_ids = obj_ids[1:]
-        # # split the color-encoded mask into a set
-        # # of binary masks
-
-        # masks = mask == obj_ids[:, None, None]
-        # num_objs = len(obj_ids)
-
-        # # convert everything into a torch.Tensor
-        # labels = torch.ones((num_objs,), dtype=torch.int64)
-        # masks = torch.as_tensor(masks, dtype=torch.float32)
-
-        # image_id = torch.tensor([idx])
-
-        # # suppose all instances are not crowd
-        # iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
-
-        # target = {}
-        # target["labels"] = labels
-        # target["masks"] = masks
-        # target["image_id"] = image_id
-        # target["iscrowd"] = iscrowd
-
-        # if self.transforms is not None:
-            # img = self.transforms(img)
-
-        # return img, target
-
-
-    # def __len__(self):
-        # return len(self.imgs)
---
+++ training/scripts/compute_dataset_normalization_parameters.py
@@ -0,0 +1,55 @@
+"""Script to get mean and standard deviation of all
+images in sugar beet dataset.
+
+Author: Jan Quakernack
+"""
+
+import click
+from PIL import Image
+import numpy as np
+
+from training.dataloader import SugarBeetDataset
+
+
+@click.command()
+def compute_dataset_normalization_parameters():
+    dataset = SugarBeetDataset.from_config()
+    filenames = dataset.filenames
+    num_images = len(filenames)
+
+    mean = np.zeros((4,), dtype=np.longdouble)
+
+    with click.progressbar(filenames, label='Reading images.') as bar:
+        for filename in bar:
+            rgb_image = np.array(Image.open(dataset.get_path_to_rgb_image(filename)).convert('RGB'))/255.0
+            nir_image = np.array(Image.open(dataset.get_path_to_nir_image(filename)).convert('L'))/255.0
+
+            mean[:3] += np.mean(rgb_image.reshape(-1, 3), axis=0)
+            mean[3] += np.mean(nir_image)
+
+    mean /= num_images
+
+    variance = np.zeros((4,), dtype=np.longdouble)
+
+    with click.progressbar(filenames, label='Reading images.') as bar:
+        for filename in bar:
+            rgb_image = np.array(Image.open(dataset.get_path_to_rgb_image(filename)).convert('RGB'))/255.0
+            nir_image = np.array(Image.open(dataset.get_path_to_nir_image(filename)).convert('L'))/255.0
+
+            variance[:3] += np.mean(np.square(rgb_image.reshape(-1, 3)-mean[:3].reshape(1, 3)), axis=0)
+            variance[3] += np.mean(np.square(nir_image-mean[3]))
+
+    variance /= num_images
+    std = np.sqrt(variance)
+
+    click.echo('mean_rgb: [{}, {}, {}]'.format(*list(mean[:3])))
+    click.echo('mean_nir: {}'.format(mean[3])
+    click.echo('std_rgb: [{}, {}, {}]'.format(*list(std)))
+    click.echo('std_nir: {}'.format(std[3])
+
+    click.echo('Done.')
+
+
+if __name__=='__main__':
+    compute_dataset_normalization_parameters()
+
