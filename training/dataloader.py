a1d2b2785650cf384e8c66988e34832dd41c3e66
jan <s7jaquak@uni-bonn.de>
2019-10-29 12:53:49 +0100

    Checkout recent changes

--- training/dataloader.py
+++ training/dataloader.py
@@ -6,6 +6,7 @@ Authors: Yung-Yu Chen, Jan Quakernack
 import numpy as np
 import torch
 from PIL import Image
+from utils import visualize
 from pathlib import Path
 
 from training import load_config, DATA_DIR
@@ -170,9 +171,10 @@ class SugarBeetDataset(Dataset):
         """Remap class labels.
         """
         semantic_target = np.zeros_like(semantic_label)
-        semantic_target[np.where(semantic_label)==self.label_ignore] = 1
-        semantic_target[np.where(semantic_label)==self.label_weed] = 2
-        semantic_target[np.where(semantic_label)==self.label_sugar_beet] = 3
+        # semantic_target = np.where(semantic_label==self.label_ignore, 1, semantic_target)
+        semantic_target = np.where(semantic_label==self.label_weed, 1, semantic_target)
+        semantic_target = np.where(semantic_label==self.label_sugar_beet, 2, semantic_target)
+
         return semantic_target
 
 
@@ -185,3 +187,61 @@ class SugarBeetDataset(Dataset):
 
     def __len__(self):
         return len(self.filenames)
+
+
+# class SugarBeetDataset(object):
+
+    # def __init__(self, root, transforms):
+        # self.root = root
+        # self.transforms = transforms
+        # # load all image files, sorting them to
+        # # ensure that they are aligned
+        # self.imgs = list(sorted(os.listdir(os.path.join(root, "images/rgb"))))
+
+
+    # def __getitem__(self, idx):
+        # # load images ad masks
+        # img_path = os.path.join(self.root, "images/rgb/", self.imgs[idx])
+        # mask_path = os.path.join(self.root, "annotations/PNG/pixelwise/iMap", "{0}_GroundTruth_iMap.png".format(self.imgs[idx].split(".")[0]))
+
+        # img = Image.open(img_path).convert("RGB")
+        # img = np.array(img)
+        # # note that we haven't converted the mask to RGB,
+        # # because each color corresponds to a different instance
+        # # with 0 being background
+        # mask = Image.open(mask_path)
+        # # convert the PIL Image into a numpy array
+        # mask = np.array(mask)
+        # # instances are encoded as different colors
+        # obj_ids = np.array([0,1, 10000, 20001])
+        # # first id is the background, so remove it
+        # obj_ids = obj_ids[1:]
+        # # split the color-encoded mask into a set
+        # # of binary masks
+
+        # masks = mask == obj_ids[:, None, None]
+        # num_objs = len(obj_ids)
+
+        # # convert everything into a torch.Tensor
+        # labels = torch.ones((num_objs,), dtype=torch.int64)
+        # masks = torch.as_tensor(masks, dtype=torch.float32)
+
+        # image_id = torch.tensor([idx])
+
+        # # suppose all instances are not crowd
+        # iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        # target = {}
+        # target["labels"] = labels
+        # target["masks"] = masks
+        # target["image_id"] = image_id
+        # target["iscrowd"] = iscrowd
+
+        # if self.transforms is not None:
+            # img = self.transforms(img)
+
+        # return img, target
+
+
+    # def __len__(self):
+        # return len(self.imgs)
--- training/utils.py
+++ training/utils.py
@@ -1,5 +1,5 @@
 from matplotlib import pyplot as plot
-
+import numpy as np
 
 def visualize(image, mask):
     plot.figure(1)
@@ -9,3 +9,33 @@ def visualize(image, mask):
     plot.subplot(212)
     plot.imshow(mask)
     plot.show() 
+
+
+def intersectionAndUnion(imPred, imLab, numClass):
+    imPred = np.asarray(imPred).copy()
+    imLab = np.asarray(imLab).copy()
+
+    imPred += 1
+    imLab += 1
+    # Remove classes from unlabeled pixels in gt image.
+    # We should not penalize detections in unlabeled portions of the image.
+    imPred = imPred * (imLab > 0)
+
+    # Compute area intersection:
+    intersection = imPred * (imPred == imLab)
+    (area_intersection, _) = np.histogram(
+        intersection, bins=numClass, range=(1, numClass))
+
+    # Compute area union:
+    (area_pred, _) = np.histogram(imPred, bins=numClass, range=(1, numClass))
+    (area_lab, _) = np.histogram(imLab, bins=numClass, range=(1, numClass))
+    area_union = area_pred + area_lab - area_intersection
+
+    return (area_intersection, area_union)
+
+def accuracy(preds, label):
+    valid = (label >= 0)
+    acc_sum = (valid * (preds == label)).sum()
+    valid_sum = valid.sum()
+    acc = float(acc_sum) / (valid_sum + 1e-10)
+    return acc, valid_sum
