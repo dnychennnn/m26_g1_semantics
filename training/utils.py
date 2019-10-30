dddcc95a71c4ecd09002f2ebc576f3029d8fddfa
denny1232 <denny1232@pop-os.localdomain>
2019-10-30 12:07:57 +0100

    Extract confidence methods

--- training/utils.py
+++ training/utils.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plot
 import numpy as np
+import cv2
 
 def visualize(image, mask):
     plot.figure(1)
@@ -9,6 +10,21 @@ def visualize(image, mask):
     plot.subplot(212)
     plot.imshow(mask)
     plot.show() 
+
+def get_confidence_map(confidence):
+    min_confidence = np.min(confidence)
+    max_confidence = np.max(confidence)
+    print('confidence min, max =', min_confidence, max_confidence)
+
+    confidence_map = (255.0*(confidence-min_confidence/(max_confidence-min_confidence+0.0001))).astype(np.uint8)
+    return confidence_map
+
+
+def visualize_single_confidence(cv_input, cv_target, cv_confidence):
+    cv2.imshow('taget sugar beet', cv_target)
+    cv2.imshow('input', cv_input)
+    cv2.imshow('sugar beet', cv_confidence)
+    cv2.waitKey(1)
 
 
 def intersectionAndUnion(imPred, imLab, numClass):
